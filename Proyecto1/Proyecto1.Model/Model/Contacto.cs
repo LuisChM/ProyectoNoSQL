d9197dcfb0272f131900658705c684ed30ea8a60
Luis <luisfelipechavam1498@gmail.com>
2020-08-26 00:11:26 -0600

    Modelo de la colecion administrativo agregado

---
+++ Proyecto1/Proyecto1.Model/Model/Contacto.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Proyecto1.Model
+{
+    [BsonIgnoreExtraElements]
+    public class Contacto
+    {
+
+        [BsonElement("email")]
+        public string email { get; set; }
+
+        [BsonExtraElements]
+        public BsonDocument Metadata { get; set; }
+    }
+}
